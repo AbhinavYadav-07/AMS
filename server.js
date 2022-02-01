86569d352b018aecffae2ac61263fc44cb3c2008
affanhussain101 <affanhussain100@gmail.com>
2022-02-02 04:51:10 +0530

    3rd

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ app.use(express.static('public'));
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
+//connecting database
+
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database:"DBMSP"
+    database:"apartment"
   });
   
   con.connect(function(err) {
@@ -21,6 +23,50 @@ var con = mysql.createConnection({
     else
     console.log("Connected!");
   });
+
+// Rendering the pages
+
+app.get('', function (req, res) {
+  res.render(__dirname+"/views/login.ejs");
+     })
+app.get('/Admin',function(req,res){
+  res.render("Admin");
+     })
+app.get('/guardLogin',function(req,res){
+      res.render("guardLogin");
+  })
+app.get('/myprofile',function(req,res){
+  res.render("myprofile");
+})
+app.get('/event',function(req,res){
+  res.render("event");
+})
+ app.get('/GuardAccount',function(req,res){
+  res.render("GuardAccount");
+})
+ app.get('/GuardProfile',function(req,res){
+  res.render("GuardProfile");
+})
+ app.get('/Gurad',function(req,res){
+  res.render("Gurad");
+})
+ app.get('/profile',function(req,res){
+  res.render("profile");
+})
+ app.get('/TenantUpdate',function(req,res){
+  res.render("TenantUpdate");
+})
+ app.get('/visitor',function(req,res){
+  res.render("visitor");
+})
+ app.get('/Account',function(req,res){
+  res.render("Account");
+})
+ app.get('/adminLogin',function(req,res){
+  res.render("adminLogin");
+})
+
+//listerner
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
