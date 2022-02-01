c22d24177025efbd06aa97f4d32350300c07b8ea
affanhussain101 <affanhussain100@gmail.com>
2022-02-02 02:14:56 +0530

    2nd

--- server.js
+++ server.js
@@ -9,7 +9,18 @@ app.use(express.static('public'));
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database:"DBMSP"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    else
+    console.log("Connected!");
+  });
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
